01fe1bdffb64e36cd7e1efa5d70b0a4cd55b9870
Yatharth <yatharthchouhan22@gmail.com>
2025-07-17 17:22:42 +0530

    code

---
+++ newrockpapergame.py
@@ -0,0 +1,47 @@
+continue_check=True
+while continue_check:
+	i=1
+
+	while i<=3:
+		
+		player1=input(" rock,  paper,  scissor:")
+		player2=input("rock,  paper, scissor:")
+
+		if player1=="rock" and player2=='paper':
+			print("player2 wins")
+		elif player1=='rock' and player2=='scissor':
+			print("player 1 wins")
+		elif player1=='rock' and player2=='rock':
+			print("tie")
+		elif player1=="paper" and player2=='rock':
+			print("player1 wins")
+		elif player1=='paper' and player2=='scissor':
+			print("player 2 wins")
+		elif player1=='paper' and player2=='paper':
+			print("tie")
+		elif player1=="scissor" and player2=='paper':
+			print("player1 wins")
+		elif player1=='scissor' and player2=='rock':
+			print("player 2 wins")
+		elif player1=='scissor' and player2=='scissor':
+			print("tie")
+
+		else :
+			print("invalid syntax")
+		i+=1
+	choice=input("press Y to continueor N to stop :")
+	if choice=="Y":
+		contiune_check=True
+		print("lETS play again")
+	else:
+		continue_check = False
+		print(" the game has ended   ")
+
+
+
+
+
+
+
+
+
